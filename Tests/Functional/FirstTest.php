24e83eb66c63c6d2ebb45e4d4849f1196ebb3a3b
Aleksandar Dimitrov <netbull2007@gmail.com>
2017-02-08 11:12:50 +0200

    adjust the config

---
+++ Tests/Functional/FirstTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Netbull\Mpay24Bundle\Tests\Functional;
+
+/**
+ * Class JsonToFormDecoderTest
+ * @package Netbull\Mpay24Bundle\Tests\Functional
+ */
+class FirstTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * First test case
+     */
+    public function testFirst()
+    {
+        $this->assertEquals('test', 'test');
+    }
+}
